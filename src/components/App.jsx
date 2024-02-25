cf26d8f2f73d140c2d64543dd8111ac8933c4138
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-02-25 09:01:22 -0700

    Sperate conditional ternary expressions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@ import Login from "./Login.jsx";
 
 var isLoggedIn = true;
 
+const currentTime = new Date(2020, 11, 1, 18).getHours();
+
 function App() {
   // Ternary operator: allows if/else statements to be condensed to an inline expression
   // CONDITION ? DO IF TRUE : DO IF FALSE
 
   return (
     <div className="container">
-      
       {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {currentTime > 17 ? <h1>Why are you still working?</h1> : null}
     </div>
   );
 }
