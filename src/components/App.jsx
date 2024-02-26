597a30966a8f735592ae37a5e54b790e7f04a8cb
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-02-25 19:59:42 -0700

    Conditional formatting with props across components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Login from "./Login.jsx";
+import Form from "./Form.jsx";
 
-var isLoggedIn = true;
+var userIsRegistered = true
 
 const currentTime = new Date(2020, 11, 1, 18).getHours();
 
@@ -11,8 +11,7 @@ function App() {
 
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {currentTime > 17 && <h1>Why are you still working?</h1>}
+    {<Form userIsRegistered={userIsRegistered}/>}
     </div>
   );
 }
