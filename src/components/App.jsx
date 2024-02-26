f292e76ada4d22bbe797daa12fe6b5af47875c5f
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-02-26 07:46:31 -0700

    Button added to toggle the useState hook for `userIsRegistered`
    the state[1] method calls the toggle of state[0], `userIsRegistered`, and uses conditional rendering to change the layout. An h1 also toggles it's contents, using the **ternary** operator, to communicate the state of userIsRegistered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form.jsx";
 
-var userIsRegistered = true
 
-const currentTime = new Date(2020, 11, 1, 18).getHours();
 
 function App() {
   // Ternary operator: allows if/else statements to be condensed to an inline expression
   // CONDITION ? DO IF TRUE : DO IF FALSE
 
+  const [userIsRegistered, toggleRegistered] = useState(0);
+
+const currentTime = new Date(2020, 11, 1, 18).getHours();
+
+function toggle() {
+  toggleRegistered(!userIsRegistered);
+}
+
   return (
     <div className="container">
+        <button id="toggle" onClick={toggle}>useState: Click to toggle <p>userIsRegistered</p></button>
+    {userIsRegistered ? <h1>Welcome back!</h1> : <h1>Nice to meet you!</h1>}
     {<Form userIsRegistered={userIsRegistered}/>}
     </div>
   );
